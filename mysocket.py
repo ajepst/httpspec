73d5a27fd37657ab3ffe99734e2a53e20cdf567a
Anne Epstein <anne@headspring.com>
2011-01-25 01:34:23 -0600

    handles non-chunked, and a little less sucky. next: clean up chunked

--- mysocket.py
+++ mysocket.py
@@ -17,16 +17,57 @@ s.send('GET / HTTP/1.1\r\nHost:www.slicehost.com\r\n\r\n')
 
 #while data: 
 #    starttime = time.time()
-#    data = s.recv(100)
+#data = s.recv(500)
 #    fullString += data
-#    print data
+#print data
     #print "received %d bytes in %d secs " %(len(data),time.time()-starttime) 
 #    data =  s.recv(100)
 #s.close()
 #print fullString[:512] 
 
+#for c in iter(lambda: f.read(1),'\n'):
+#        pass
+
+def getLine(s):
+    print "getline"
+    line = '' 
+    for l in iter(lambda: s.recv(1), '\r'):
+        if ((l != '\n') and (l != '\r')):
+            line += l
+    return line
+
+def getHeader(s):
+    header =[] 
+    for line in iter(lambda:getLine(s), ''):
+        header.append(line) 
+    return header
+
+def getContentLength(header):
+    for line in header: 
+        if (re.match("Content-Length: \d+$", line)):
+            return int(line[line.find(": ")+2 : len(line)])
+    return -1
+
+def safeGet(s, contentLength):
+    chunkLength = 1024
+    received ='' 
+    while (len(received) + chunkLength) < contentLength:
+        chunk = s.recv(chunkLength)
+        received += chunk
+    remaining = contentLength - len(received)
+    received += s.recv(remaining)
+    return received
 
 def fetchlines(s):
+
+    header = getHeader(s)
+    contentLength = getContentLength(header)   
+    if contentLength > 0:
+        #full content chunk
+        print "BELOW"
+        return safeGet(s, contentLength)
+
+
     print "in fetchlines"
     myline = ''
     data = s.recv(2000)
@@ -38,6 +79,9 @@ def fetchlines(s):
     linesToReturn = "" 
     skipNextLine = False
     firstGroup = True
+
+    
+
     while data:
         if '\n' in data:
              lines = data.split("\r\n")
