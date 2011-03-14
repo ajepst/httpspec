f498ded41e6e68edd115de18fd672fd17c0de852
Anne Epstein <ajepst@gmail.com>
2011-03-13 19:06:12 -0500

    separated out reading the status

--- mysocket.py
+++ mysocket.py
@@ -44,32 +44,23 @@ def get_url(unparsed_url):
 
 
 def fetch_content(sock):
-    header = get_header(sock)
-    status_code = get_status_code(header)
+    status_code = get_status_code(sock)
     if status_code < 200 or status_code > 299:
         return get_special_message(status_code)
-    content_length = get_content_length(header)
+    headers = get_headers(sock)
+    content_length = get_content_length(headers)
     if content_length > 0:
         #full content chunk
         return safe_get(sock, content_length)
     # continue here, we are chunked into pieces
-    # for now put loop here, extract out getting the count
-    content = ""
-    chunk_length = int(get_line(sock), 16)
-    while (chunk_length > 0):
-        content += safe_get(sock, chunk_length)
-        chunk_length = int(find_next_non_empty_line(sock), 16)
-    return content
+    return get_by_chunks(sock)
 
 
-def get_header(sock):
+def get_headers(sock):
     header = {}
     for line in iter(lambda: get_line(sock), ''):
         line_items = line.split(': ')
-        if len(line_items) == 1:
-            header['status'] = line_items[0]
-        else:
-            header[string.lower(line_items[0])] = line_items[1]
+        header[string.lower(line_items[0])] = line_items[1]
     return header
 
 
@@ -81,11 +72,10 @@ def get_line(sock):
     return line
 
 
-def get_status_code(header):
-    if 'status' in header:
-        matches = re.search("^HTTP/\d+\.\d+ (\d{3})", header['status'])
-        return int(matches.group(1))
-    return -1
+def get_status_code(sock):
+    line = get_line(sock)
+    matches = re.search("^HTTP/\d+\.\d+ (\d{3})", line)
+    return int(matches.group(1))
 
 def get_special_message(code):
     if code >= 300 and code < 400:
@@ -120,6 +110,14 @@ def safe_get(sock, content_length):
         received += extra
     return received
 
+
+def get_by_chunks(sock):
+    content = ""
+    chunk_length = int(get_line(sock), 16)
+    while (chunk_length > 0):
+        content += safe_get(sock, chunk_length)
+        chunk_length = int(find_next_non_empty_line(sock), 16)
+    return content
 
 if __name__ == "__main__":
     main()
